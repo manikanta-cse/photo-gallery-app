74363429daccc9cd1677494f8ebdbf6eb646ae84
Kattamuri, Manikanta <Manikanta.Kattamuri@cdk.com>
2018-10-08 14:24:27 +0530

    node server setup

---
+++ server.js
@@ -0,0 +1,16 @@
+'use strict'
+
+
+var express = require( 'express' );
+var app = express();
+var port = process.env.PORT || 8090;
+
+app.get( '/', function( req, res ) {
+  
+  res.sendFile( __dirname + '/index.html');
+  
+});
+
+var server = app.listen( port, function() {
+  console.log( 'Server started, listening on ' + port );
+})
